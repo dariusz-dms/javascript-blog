0957ac2afa34ac091379ea14e9ab72e1d32e9a08
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2023-12-01 02:13:55 +0100

    add_JS

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Pobierz wszystkie linki do artykułów
+const links = document.querySelectorAll('.article-link');
+
+// Iteruj przez każdy link i dodaj nasłuchiwanie zdarzenia 'click'
+links.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); // Zapobiegaj domyślnemu zachowaniu linku
+
+        // Usuń klasę 'active' ze wszystkich linków
+        links.forEach(link => {
+            link.classList.remove('active');
+        });
+
+        // Dodaj klasę 'active' do klikniętego linka
+        this.classList.add('active');
+
+        // Ukryj wszystkie artykuły (usunięcie klasy 'active')
+        const articles = document.querySelectorAll('.article');
+        articles.forEach(article => {
+            article.classList.remove('active');
+        });
+
+        // Pobierz wartość atrybutu 'href' z klikniętego linka
+        const articleId = this.getAttribute('href');
+
+        // Znajdź artykuł o odpowiednim ID
+        const selectedArticle = document.querySelector(articleId);
+
+        // Dodaj klasę 'active' do znalezionego artykułu
+        selectedArticle.classList.add('active');
+    });
+});
