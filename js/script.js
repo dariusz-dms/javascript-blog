3b6c3451cb85f43ff5f7a3dcc0e25baf8c4fc7fb
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2023-12-03 08:14:40 +0100

    add-fix

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
-// Pobierz wszystkie linki do artykułów
-const links = document.querySelectorAll('.article-link');
-
-// Pobierz wszystkie linki po kliknięciu przycisku
-document.getElementById('test-button').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function() {
+    const titleClickHandler = function(event){
+      event.preventDefault(); // Zapobiegaj domyślnemu zachowaniu linku
+  
+      const clickedElement = this;
+      console.log('Link was clicked!');
+      console.log(clickedElement);
+  
+      // Usuń klasę 'active' ze wszystkich linków
+      const links = document.querySelectorAll('.titles a');
+      links.forEach(link => {
+        link.classList.remove('active');
+      });
+  
+      // Dodaj klasę 'active' do klikniętego linka
+      clickedElement.classList.add('active');
+  
+      // Ukryj wszystkie artykuły (usunięcie klasy 'active')
+      const articles = document.querySelectorAll('.post');
+      articles.forEach(article => {
+        article.classList.remove('active');
+      });
+  
+      // Pobierz wartość atrybutu 'href' z klikniętego linka
+      const articleId = clickedElement.getAttribute('href');
+  
+      // Znajdź artykuł o odpowiednim ID
+      const selectedArticle = document.querySelector(articleId);
+  
+      // Dodaj klasę 'active' do znalezionego artykułu
+      selectedArticle.classList.add('active');
+    };
+  
     const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-});
-
-// Iteruj przez każdy link i dodaj nasłuchiwanie zdarzenia 'click'
-links.forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault(); // Zapobiegaj domyślnemu zachowaniu linku
-
-        // Usuń klasę 'active' ze wszystkich linków
-        links.forEach(singleLink => {
-            singleLink.classList.remove('active');
-        });
-
-        // Dodaj klasę 'active' do klikniętego linka
-        this.classList.add('active');
-
-        // Ukryj wszystkie artykuły (usunięcie klasy 'active')
-        const articles = document.querySelectorAll('.article');
-        articles.forEach(article => {
-            article.classList.remove('active');
-        });
-
-        // Pobierz wartość atrybutu 'href' z klikniętego linka
-        const articleId = this.getAttribute('href');
-
-        // Znajdź artykuł o odpowiednim ID
-        const selectedArticle = document.querySelector(articleId);
-
-        // Dodaj klasę 'active' do znalezionego artykułu
-        selectedArticle.classList.add('active');
-    });
-});
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
+  });
