ec2f6016e22220381b2cee683c85257ff85e2b10
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2023-12-08 00:55:09 +0100

    add_padding

--- js/script.js
+++ js/script.js
@@ -128,6 +128,7 @@ function generateTags() {
 
 generateTags();
 
+// Handle click events on tags
 function tagClickHandler(event){
   // prevent default action for this event
   event.preventDefault();
@@ -151,7 +152,7 @@ function tagClickHandler(event){
   });
   // END LOOP: for each active tag link
   // find all tag links with "href" attribute equal to the "href" constant
-  const sameTagLinks = document.querySelectorAll('a[href="' + href + '"]');
+  const sameTagLinks = document.querySelectorAll(`a[href="${href}"]`);
 
   // START LOOP: for each found tag link
   sameTagLinks.forEach(tagLink => {
@@ -161,7 +162,7 @@ function tagClickHandler(event){
   // END LOOP: for each found tag link
 
   // execute function "generateTitleLinks" with article selector as argument
-  generateTitleLinks('[data-tags~="' + tag + '"]');
+  generateTitleLinks(`[data-tags~="${tag}"]`);
 
 }
 // find all links to tags
@@ -175,6 +176,9 @@ function addClickListenersToTags(){
   });
   // END LOOP: for each link
 }
+  // Call generateTags function after the HTML structure is loaded
+  document.addEventListener('DOMContentLoaded', function() {
+  generateTags();
 
 addClickListenersToTags();
 
@@ -224,6 +228,11 @@ function authorClickHandler(event) {
   // Generate title links based on the author attribute and display them
   generateTitleLinks(`[data-author="${author}"]`);
 }
+
+  // Call generateTags function after the HTML structure is loaded
+document.addEventListener('DOMContentLoaded', function() {
+  generateTags();
+});
 
 generateAuthors();
 addClickListenersToAuthors();
